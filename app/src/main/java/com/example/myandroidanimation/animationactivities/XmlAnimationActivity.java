5e415820d295075cf1edd5ce1a33b33bfbdcadbb
DarushDev <darushfaqiri@gmail.com>
2017-09-13 20:20:34 +0800

    Jump and blink (Animations in XML)

--- app/src/main/java/com/example/myandroidanimation/animationactivities/XmlAnimationActivity.java
+++ app/src/main/java/com/example/myandroidanimation/animationactivities/XmlAnimationActivity.java
@@ -1,8 +1,28 @@
 package com.example.myandroidanimation.animationactivities;
 
-public class XmlAnimationActivity extends BaseAnimationActivity {
-  @Override
-  protected void onStartAnimation() {
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorSet;
 
-  }
+import com.example.myandroidanimation.R;
+
+public class XmlAnimationActivity extends BaseAnimationActivity {
+    @Override
+    protected void onStartAnimation() {
+        // 1
+        AnimatorSet rocketAnimatorSet = (AnimatorSet) AnimatorInflater.loadAnimator(this, R.animator.jump_and_blink);
+        // 2
+        rocketAnimatorSet.setTarget(mRocket);
+
+        // 3
+        AnimatorSet dogeAnimatorSet = (AnimatorSet) AnimatorInflater.loadAnimator(this, R.animator.jump_and_blink);
+        // 4
+        dogeAnimatorSet.setTarget(mDoge);
+
+        // 5
+        AnimatorSet bothAnimatorSet = new AnimatorSet();
+        bothAnimatorSet.playTogether(rocketAnimatorSet, dogeAnimatorSet);
+        // 6
+        bothAnimatorSet.setDuration(DEFAULT_ANIMATION_DURATION);
+        bothAnimatorSet.start();
+    }
 }
