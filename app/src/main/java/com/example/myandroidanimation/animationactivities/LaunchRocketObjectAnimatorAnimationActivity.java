15f28c5092941f66a641193f0325726aab62c028
DarushDev <darushfaqiri@gmail.com>
2017-09-13 19:13:47 +0800

    Launch rocket using ObjectAnimator class

--- app/src/main/java/com/example/myandroidanimation/animationactivities/LaunchRocketObjectAnimatorAnimationActivity.java
+++ app/src/main/java/com/example/myandroidanimation/animationactivities/LaunchRocketObjectAnimatorAnimationActivity.java
@@ -1,8 +1,14 @@
 package com.example.myandroidanimation.animationactivities;
 
-public class LaunchRocketObjectAnimatorAnimationActivity extends BaseAnimationActivity {
-  @Override
-  protected void onStartAnimation() {
+import android.animation.ObjectAnimator;
 
-  }
+public class LaunchRocketObjectAnimatorAnimationActivity extends BaseAnimationActivity {
+    @Override
+    protected void onStartAnimation() {
+        // 1
+        ObjectAnimator objectAnimator = ObjectAnimator.ofFloat(mRocket, "translationY", 0, -mScreenHeight);
+        // 2
+        objectAnimator.setDuration(DEFAULT_ANIMATION_DURATION);
+        objectAnimator.start();
+    }
 }
